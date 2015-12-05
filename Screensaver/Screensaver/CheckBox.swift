4e78976f5c2f0f17da65b674577123b3a47f808c
Michelle Leon <michelle.leon@berkeley.edu>
2015-12-04 23:54:59 -0800

    Changed to high res photos, implemented setAlarm vc

---
+++ Screensaver/Screensaver/CheckBox.swift
@@ -0,0 +1,21 @@
+//
+//  CheckBox.swift
+//  Screensaver
+//
+//  Created by Michelle Leon on 12/4/15.
+//  Copyright © 2015 Michelle Leon. All rights reserved.
+//
+
+import UIKit
+
+class CheckBox: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
